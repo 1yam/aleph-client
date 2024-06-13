426166a1797b91c87a0bf0ab07298ec3b705ca8a
lyam <lyam.gomes@epitech.eu>
2024-06-13 14:56:39 +0200

    vesrion

--- src/aleph_client/commands/account.py
+++ src/aleph_client/commands/account.py
@@ -129,7 +129,7 @@ def sign_bytes(
 
     coroutine = account.sign_raw(message.encode())
     signature = asyncio.run(coroutine)
-    typer.echo(f"0x{signature.hex()}")
+    typer.echo(signature.hex())
 
 
 @app.command()
